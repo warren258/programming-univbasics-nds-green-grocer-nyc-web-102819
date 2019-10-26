b6153c9948d46dcac75ce4ab0fd8762023ef7335
Warren McDonald <warrenjmac@mail.com>
2019-10-26 15:58:45 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -2,12 +2,13 @@ def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
+   
 end
 
 def consolidate_cart(cart)
   # Consult README for inputs and outputs
   #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
+  # REMEMBER: This returns a new Array that represents the cart. Don't  # merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
 end
 
@@ -33,4 +34,5 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
+   
 end
